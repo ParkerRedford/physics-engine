3ce7869dcb9d14a084c3d10bd7ef22f0a6137849
Parker Redford <redrevyol@gmail.com>
2025-07-03 19:43:52 -0400

    added to be modified later

---
+++ src/math_functions/trigonometry.rs
@@ -0,0 +1,64 @@
+fn sin(degrees: u8, accuracy: i8) -> u64 {
+
+    0
+}
+
+fn arcsin(degrees: u8, accuracy: i8) -> u64 {
+    
+    0
+}
+
+fn csc(degrees: u8, accuracy: i8) -> u64 {
+    
+    0
+}
+
+fn arccsc(degrees: u8, accuracy: i8) -> u64 {
+
+    0
+}
+
+fn cos(degrees: u8, accuracy: i8) -> u64 {
+    
+    0
+}
+
+fn arccos(degrees: u8, accuracy: i8) -> u64 {
+    
+    0
+}
+
+fn sec(degrees: u8, accuracy: i8) -> u64 {
+
+    0
+}
+
+fn arcsec(degrees: u8, accuracy: i8) -> u64 {
+
+    0
+}
+
+fn tan(degrees: u8, accuracy: i8) -> u64 {
+    
+    0
+}
+
+fn arctan(degrees: u8, accuracy: i8) -> u64 {
+    
+    0
+}
+
+fn cot(degrees: u8, accuracy: i8) -> u64 {
+
+    0
+}
+
+fn arccot(degrees: u8, accuracy: i8) -> u64 {
+
+    0
+}
+
+fn ln(value: u8, accuracy: i8) -> u64 {
+
+    0
+}
